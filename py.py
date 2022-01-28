ac8d5b2e7e2d6a9a239b1fb4d618080a4c97161d
Ardit Berisha <88629825+ardit-cpu@users.noreply.github.com>
2022-01-28 23:05:23 +0100

    Add files via upload

---
+++ py.py
@@ -0,0 +1,9 @@
+from main import block
+from test import chain
+
+if(chain == "main"):
+	print("block chain is Active")
+else:
+	def momentum():
+		print("to start the blcok cain begin please you need to turn on")
+		momentum()
